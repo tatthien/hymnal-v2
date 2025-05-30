cb224f2468a8a408c0c9a998163d78e2d2978da8
Thien Nguyen <tatthien.contact@gmail.com>
2025-05-30 10:38:06 +0700

    feat: add menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,12 +29,18 @@ export default function RootLayout({
           'antialiased leading-normal max-w-[600px] p-4 mx-auto'
         ].join(' ')}
       >
-        <header className="mb-10">
+        <header className="mb-6">
           <div>
             <Link href='/' className="font-bold text-lg leading-snug">Hymnal</Link>
             <span className="block text-sm text-gray-600">a &quot;digital&quot; book of hymns</span>
           </div>
         </header>
+
+        <nav className="mb-6 flex gap-4">
+          <Link href='/hymns' className="text-sm text-gray-600 hover:underline hover:text-gray-900">Thánh Ca</Link>
+          <Link href='/tvchh' className="text-sm text-gray-600 hover:underline hover:text-gray-900">Tôn Vinh Chúa Hằng Hữu</Link>
+        </nav>
+
         <main>
           {children}
         </main>
